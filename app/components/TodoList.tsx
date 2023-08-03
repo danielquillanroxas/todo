3a7570cd2b444d7631ab4563eb0891fd6d467ffc
danielquillanroxas <casrox54@yahoo.com>
2023-08-03 15:04:54 +0300

    new changes

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
-import { Dialog } from 'primereact/dialog';
-import axios from 'axios';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import { useEffect, useState } from 'react'
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
+import { Button } from 'primereact/button'
+import { Dialog } from 'primereact/dialog'
+import 'primereact/resources/primereact.min.css'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+import { v4 as uuidv4 } from 'uuid'
 
 const TodoList = () => {
-  const [griddata, setGridData] = useState([]);
+  const [griddata, setGridData] = useState(() => {
+    const localData = localStorage.getItem('tasks');
+    return localData ? JSON.parse(localData) : [];
+  });
   const [visible, setVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
   const [newTask, setNewTask] = useState({
@@ -17,45 +21,29 @@ const TodoList = () => {
   });
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
-
-  const fetchTasks = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/tasks');
-      setGridData(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+    localStorage.setItem('tasks', JSON.stringify(griddata));
+  }, [griddata]);
 
   const handleEdit = (rowData) => {
     setSelectedItem(rowData);
     setVisible(true);
   };
 
-  const handleEditSubmit = async (updatedItem) => {
-    try {
-      await axios.put(`http://localhost:3001/tasks/${updatedItem.id}`, updatedItem);
-      setVisible(false);
-      fetchTasks();
-    } catch (error) {
-      console.error('Error updating item:', error);
-    }
+  const handleEditSubmit = (updatedItem) => {
+    setGridData(prevData => prevData.map(item => item.id === updatedItem.id ? updatedItem : item));
+    setVisible(false);
+    setSelectedItem(null);
   };
 
-  const handleDelete = async () => {
-    try {
-      await axios.delete(`http://localhost:3001/tasks/${selectedItem.id}`);
-      setVisible(false);
-      fetchTasks();
-    } catch (error) {
-      console.error('Error deleting item:', error);
-    }
+  const handleDelete = () => {
+    setGridData(prevData => prevData.filter(item => item.id !== selectedItem.id));
+    setVisible(false);
+    setSelectedItem(null);
   };
 
   const handleCancelEdit = () => {
     setSelectedItem(null);
+    setNewTask({ name: '', description: '' });
     setVisible(false);
   };
 
@@ -64,22 +52,16 @@ const TodoList = () => {
     setVisible(true);
   };
 
-  const handleAddNewTaskSubmit = async () => {
-    try {
-      const response = await axios.post('http://localhost:3001/tasks', newTask);
-      const addedTask = response.data;
-      setGridData((prevData) => [...prevData, addedTask]);
-      setVisible(false);
-    } catch (error) {
-      console.error('Error adding new task:', error);
-    }
+  const handleAddNewTaskSubmit = () => {
+    setGridData(prevData => [...prevData, { ...newTask, id: uuidv4() }]);
+    setVisible(false);
   };
 
   const footerContent = (
     <div>
       {selectedItem ? (
         <>
-          <Button label="Save" icon="pi pi-save" onClick={ () => handleEditSubmit({ ...selectedItem, name: document.getElementById('name').value, description: document.getElementById('description').value })} />
+          <Button label="Save" icon="pi pi-save" onClick={() => handleEditSubmit({ ...selectedItem, name: document.getElementById('name').value, description: document.getElementById('description').value })} />
           <Button label="Cancel" icon="pi pi-times" onClick={handleCancelEdit} />
           <Button label="Delete" icon="pi pi-trash" onClick={handleDelete} />
         </>
@@ -93,14 +75,14 @@ const TodoList = () => {
   );
 
   return (
-    <div>
-      <div className="text-center m-5 surface-card p-4 border-round flex align-items-center flex-column justify-content-center">
-        <span className="font-medium m-3">To-do list App</span>
-        <Button className='w-30rem m-3' label='Add Item to the List' onClick={handleAddNewTask}>
-          <i className="pi pi-plus"></i>
-        </Button>
-      </div>
-      <div className="card">
+    <div className="p-grid p-justify-center">
+      <div className="text-center m-5 surface-card p-4 border-round flex flex-column align-items-center justify-content-center">
+  <span className="font-medium m-3">To-do list App</span>
+  <Button className='custom-button m-3' label='Add Item to the List' onClick={handleAddNewTask}>
+    <i className="pi pi-plus"></i>
+  </Button>
+</div>
+      <div className="p-col-12 p-md-10 p-lg-8 card">
         <DataTable value={griddata}>
           <Column field="name" header="Task Name" />
           <Column field="description" header="Description" />
@@ -113,7 +95,6 @@ const TodoList = () => {
           />
         </DataTable>
 
-        {/* Conditional rendering for the edit modal */}
         <Dialog header={selectedItem ? 'Edit Item' : 'Add New Item'} visible={visible} style={{ width: '50vw' }} onHide={handleCancelEdit} footer={footerContent}>
           <form>
             <label htmlFor="name">Task Name: </label>
